b3d8e7a68ba8ac9544d159459f5ad61d7d9bb614
Samuel Akintunde <samakintunde37@gmail.com>
2019-07-16 16:20:04 +0100

    Built basic chat function

---
+++ client/js/main.js
@@ -0,0 +1,111 @@
+// import { openDB } from "./vendor/idb";
+
+// const db = async () =>
+//   openDB('userInfo', '1.0', {
+
+//   });
+
+const socket = io();
+
+const chatForm = document.forms["chat-form"];
+const setUserForm = document.forms["set-user-form"];
+
+const chatArea = document.querySelector(".chat");
+const userEl = document.querySelector(".header__user");
+
+const appState = {
+  name: "",
+  messages: []
+};
+
+const getMessage = () => {
+  //const message = form.message.value;
+  return chatForm.message.value;
+};
+
+const getCurrentTime = () => {
+  const d = new Date();
+  return d.toLocaleTimeString();
+};
+
+// build Message
+const renderMessage = payload => {
+  const { name, message } = payload;
+
+  const messageRow = document.createElement("div");
+  if (name === appState.name) {
+    messageRow.className = "message-container message-container--left";
+  } else {
+    messageRow.className = "message-container message-container--right";
+  }
+
+  const messageBoxEl = document.createElement("article");
+  messageBoxEl.className = "message";
+
+  const messageEl = document.createElement("p");
+  messageEl.className = "message__text";
+  messageEl.textContent = message;
+
+  const nameEl = document.createElement("p");
+  nameEl.className = "message__author";
+  nameEl.textContent = name;
+
+  const dateEl = document.createElement("p");
+  dateEl.className = "message__timestamp";
+  dateEl.textContent = getCurrentTime();
+
+  messageBoxEl.appendChild(nameEl);
+  messageBoxEl.appendChild(messageEl);
+  messageBoxEl.appendChild(dateEl);
+
+  messageRow.appendChild(messageBoxEl);
+
+  return messageRow;
+};
+
+// Receive Messages
+socket.on("syncMessages", payload => {
+  chatArea.appendChild(renderMessage(payload));
+  appState.messages.push(payload.message);
+});
+
+// console.log(location);
+
+// Send Messages
+if (location.pathname === "/chat.html") {
+  const data = JSON.parse(localStorage.getItem("userData"));
+  console.log(data);
+  userEl.textContent = appState.name;
+
+  if (data) {
+    appState.name = data.name;
+    appState.messages = [...data.messages] || [];
+    userEl.innerText = appState.name;
+  }
+
+  chatForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const name = appState.name || "Anonymous";
+    const message = getMessage();
+
+    socket.emit("sendMessage", { name, message });
+    chatForm.reset();
+  });
+}
+
+// Set Username
+if (location.pathname === "/" || location.pathname === "/index.html") {
+  setUserForm.addEventListener("submit", e => {
+    e.preventDefault();
+    appState.name = setUserForm.name.value;
+
+    const data = JSON.stringify({
+      name: appState.name,
+      messages: appState.messages
+    });
+
+    localStorage.setItem("userData", data);
+    location.pathname = "/chat.html";
+  });
+}
---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const ws = socketio(server);
+
+// Serve website
+const publicDirectoryPath = path.join(__dirname, "../client");
+app.use(express.static(publicDirectoryPath));
+
+// Generate usernames
+
+ws.on("connection", socket => {
+  console.log("A socket connected");
+
+  // socket.emit("userName", userName);
+
+  socket.on("sendMessage", payload => {
+    ws.emit("syncMessages", {
+      name: payload.name,
+      message: payload.message
+    });
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
